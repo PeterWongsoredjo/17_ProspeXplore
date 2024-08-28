fd98721e164bffd54e332e322efd34b254f1dff5
PeterWongsoredjo <167316688+PeterWongsoredjo@users.noreply.github.com>
2024-08-29 01:29:41 +0700

    Update route.js

--- app/api/userExists/route.js
+++ app/api/userExists/route.js
@@ -1,6 +1,6 @@
 import { connectMongoDB } from "@/lib/mongodb";
 import { NextResponse } from "next/server";
-import User from "@/app/models/User";
+import User from "../../models/User";
 
 export async function POST(req) {
   try {
